f536b4b7840e06e3e62a52e56e4e50807d06b5b7
Matias Gonzalez <matiasgonzalez@Matiass-MacBook-Pro-2.local>
2018-05-04 00:19:09 -0300

    fast version using numpy matrix operations

--- regression/bitcoinPrediction.py
+++ regression/bitcoinPrediction.py
@@ -5,31 +5,29 @@ from datetime import datetime
 class RegressionModel:
 
     def __init__(self, X):
-        self.learningRate = 0.0000000000000000000000000000000000000000000000001
+        self.learningRate = 1e-7
         self.tita0 = np.random.rand()
         self.tita1 = np.random.rand()
         self.size = X.count()
 
-    def hipotesis(self, x):
-        print('hipotesis {}'.format(self.tita0 + self.tita1 * x))
-        return self.tita0 + self.tita1 * x
+    def hipotesis(self, X):
+        # return self.tita0 + self.tita1 * x
+        return self.tita0 + self.tita1 * X
 
-    def updateWeights(self,x, y):
-        print('X value {}'.format(x))
-        print('Y value {}'.format(y))
-        print('tita0 {}'.format(self.tita0))
-        print('tita1 {}'.format(self.tita1))
-        newValueTita0 = (1/self.size) * (self.hipotesis(x) - y) * x
-        newValueTita1 = (1/self.size) * (self.hipotesis(x) - y)
-        print('newValueTita0 {}'.format(newValueTita0))
-        print('newValueTita1 {}'.format(newValueTita1))
-        self.tita0 = self.tita0 + self.learningRate *  newValueTita0
-        self.tita1 = self.tita1 + self.learningRate *  newValueTita1
+    def updateWeights(self,x, y, predictValue):
+        newValueTita0 = ((1/self.size) * (predictValue - y) * x).sum()
+        newValueTita1 = ((1/self.size) * (predictValue - y)).sum()
+        self.tita0 -= self.learningRate *  newValueTita0
+        self.tita1 -= self.learningRate *  newValueTita1
 
     def train(self, X, Y):
-        for i in range(self.size):
-            print("cost function: {}".format(self.costFunction(self.hipotesis(X[i]), Y[i])))
-            self.updateWeights(X[i], Y[i])
+        # for i in range(self.size):
+        #     predictValue = self.hipotesis(X[i])
+        #     print("cost function: {}".format(self.costFunction(predictValue, Y[i])))
+        #     self.updateWeights(X[i], Y[i], predictValue)
+        predictValue = self.hipotesis(X)
+        self.updateWeights(X, Y, predictValue)
+
 
     def costFunction(self, eval, y):
         return (1/2*self.size) * (eval - y)**2
@@ -37,8 +35,8 @@ class RegressionModel:
 def main():
     df = pd.read_csv('all_currencies.csv')
     btcValueByDate = df.loc[df['Symbol']=='BTC'][['Date', 'High']]
-    X = pd.to_datetime(btcValueByDate['Date']).map(datetime.toordinal)[:500]
-    Y = pd.to_numeric(btcValueByDate['High'])[:500]
+    X = pd.to_datetime(btcValueByDate['Date']).map(datetime.toordinal)
+    Y = pd.to_numeric(btcValueByDate['High'])
     Xm = X.as_matrix()
     Ym = Y.as_matrix()
     rm = RegressionModel(X)
@@ -48,13 +46,12 @@ def main():
     print('size {}'.format(rm.size))
     print('learningRate {}'.format(rm.learningRate))
 
-    for i in range(1000):
+    for i in range(10**10):
         rm.train(Xm, Ym)
 
-    print(datetime.fromordinal(Xm[499]))
-    # print(rm.costFunction(rm.hipotesis(Xm[499]), Ym[499]))
-    # rm.updateWeights(Xm[499], Ym[499])
-    print(rm.hipotesis(Xm[499]))
+    print('Predict value for: {} = {}'.format(datetime.fromordinal(Xm[499]), rm.hipotesis(Xm[499])))
+    print('Real value for: {} = {}'.format(datetime.fromordinal(Xm[499]), Ym[499]))
+
 
 
 if __name__ == "__main__":
