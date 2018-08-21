30732f6fd5d779e145f1a7f911e258714106f42f
Matias Gonzalez <matias.mart.gonzalez@gmail.com>
2018-08-21 17:13:32 -0300

    residual network

---
+++ convolutionalNetwork/keras/fashionMnist/residualNetwork.py
@@ -0,0 +1,123 @@
+import numpy as np
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.preprocessing import image
+# IMPORT MODEL
+from keras.models import load_model
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+from keras.initializers import glorot_uniform
+# import pydot
+# from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+# from kt_utils import *
+from utils import *
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+# import matplotlib.pyplot as plt
+# from matplotlib.pyplot import imshow
+from blocks import *
+
+def ResNet50(input_shape = (64, 64, 3), classes = 10):
+    """
+    Implementation of the popular ResNet50 the following architecture:
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+    -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+
+    Arguments:
+    input_shape -- shape of the images of the dataset
+    classes -- integer, number of classes
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+    ### START CODE HERE ###
+
+    # Stage 3 (≈4 lines)
+    X = convolutional_block(X, f = 3, filters = [128, 128, 512], stage = 3, block='a', s = 1)
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+    # Stage 4 (≈6 lines)
+    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 1)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    # Stage 5 (≈3 lines)
+    X = convolutional_block(X, f = 3, filters=[512, 512, 2048], stage = 5, block='a', s = 1)
+    X = identity_block(X, 3, [512, 512, 2048], stage = 5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage = 5, block='c')
+
+    # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+    # print('X shape: ', X.shape)
+    X = AveragePooling2D(pool_size=(2,2), name='avg_pool')(X)
+
+    ### END CODE HERE ###
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+
+
+    # Create model
+    model = Model(inputs = X_input, outputs = X, name='ResNet50')
+
+    return model
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--predict_image_class', help='predict for image class using the pre trainned weights')
+    parser.add_argument('--parameters', help='path of the parameters to use', default='params_model_0.h5')
+    parser.add_argument('--num_epochs', help='iteration number', default=1500)
+    parser.add_argument('--predict_all_with_params', help='probability of keeping the neuron in the dropout method')
+    args = parser.parse_args()
+
+    num_epochs = int(args.num_epochs)
+    image_path = args.predict_image_class
+    parameters = args.parameters
+    if image_path:
+        model = load_model(parameters)
+        image = load_image(image_path)
+        classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+        my_image_prediction = model.predict(x=image)
+        print("Your algorithm predicts: [" + classes[np.squeeze(np.argmax(my_image_prediction))]+']')
+        return
+
+    X_train, Y_train, X_test, Y_test = init_dataset_normalize()
+    model = ResNet50((28,28,1))
+    model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x=X_train,y=Y_train, epochs=num_epochs)
+    model.evaluate(x=X_test, y=Y_test)
+    model.save('params_model_'+str(getNext())+'.h5')
+    model.summary()
+
+main()
