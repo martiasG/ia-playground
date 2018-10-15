a2958098b650eb3c00925cf86c79f2eb633b9a10
Matias Gonzalez <matiasgonzalez@Matiass-MacBook-Pro.local>
2018-10-15 14:12:39 -0300

    using lenet5

--- convolutionalNetwork/keras/fashionMnist/model.py
+++ convolutionalNetwork/keras/fashionMnist/model.py
@@ -9,6 +9,8 @@ from keras.models import load_model
 from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
 from keras.applications.imagenet_utils import preprocess_input
+from keras.layers import ZeroPadding2D
+from keras.layers import Dropout
 # import pydot
 # from IPython.display import SVG
 from keras.utils.vis_utils import model_to_dot
@@ -23,7 +25,7 @@ K.set_image_data_format('channels_last')
 
 def ModelFashionMnis(input_shape):
     """
-    Implementation of the fashionMnistmodel.
+    Implementation of the vgg-16 network architecture.
 
     Arguments:
     input_shape -- shape of the images of the dataset
@@ -34,36 +36,44 @@ def ModelFashionMnis(input_shape):
 
     X_train_input = Input(input_shape)
 
-    X = ZeroPadding2D((3, 3))(X_train_input)
-    X = Conv2D(32, (7, 7),strides=(1, 1))(X_train_input)
+    X_padded = ZeroPadding2D(padding=2, data_format="channels_last")(X_train_input)
+
+    X = Conv2D(6, (5, 5),strides=(1, 1), padding='same')(X_padded)
     # normalize the third component colors
     X = BatchNormalization(axis=3)(X)
     X = Activation('relu')(X)
-    X = MaxPooling2D((2,2))(X)
+    X = AveragePooling2D((2,2))(X)
 
-    X = ZeroPadding2D((2, 2))(X_train_input)
-    X = Conv2D(64, (5, 5),strides=(1, 1))(X_train_input)
+    X = Conv2D(6, (5, 5),strides=(1, 1), padding='same')(X)
+    # normalize the third component colors
     X = BatchNormalization(axis=3)(X)
     X = Activation('relu')(X)
-    X = MaxPooling2D((2,2))(X)
+    X = AveragePooling2D((2,2))(X)
 
-    X = ZeroPadding2D((1, 1))(X_train_input)
-    X = Conv2D(128, (3, 3),strides=(1, 1))(X_train_input)
+    X = Conv2D(16, (5, 5),strides=(1, 1), padding='same')(X)
+    # normalize the third component colors
     X = BatchNormalization(axis=3)(X)
     X = Activation('relu')(X)
+    X = AveragePooling2D((2,2))(X)
 
     X = Flatten()(X)
+    X = Dense(120, activation='relu')(X)
+    X = Dropout(0.5)(X)
+    X = Dense(84, activation='relu')(X)
+    X = Dropout(0.5)(X)
+
     X = Dense(10, activation='softmax')(X)
     model = Model(inputs=X_train_input, outputs=X)
 
     return model
+
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--predict_image_class', help='predict for image class using the pre trainned weights')
     parser.add_argument('--parameters', help='path of the parameters to use', default='params_model_0.h5')
-    parser.add_argument('--num_epochs', help='iteration number', default=1500)
+    parser.add_argument('--num_epochs', help='iteration number', default=100)
     parser.add_argument('--predict_all_with_params', help='probability of keeping the neuron in the dropout method')
     args = parser.parse_args()
 
@@ -82,7 +92,9 @@ def main():
     model = ModelFashionMnis((28,28,1))
     model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy'])
     model.fit(x=X_train,y=Y_train, epochs=num_epochs)
-    model.evaluate(x=X_test, y=Y_test)
+    score = model.evaluate(x=X_test, y=Y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
     model.save('params_model_'+str(getNext())+'.h5')
     model.summary()
 
